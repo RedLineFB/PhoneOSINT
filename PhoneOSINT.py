513f39cfa10c4a4dc211528c140676641cde95b7
Facundo Betancur <114316952+RedLineFB@users.noreply.github.com>
2022-10-05 23:44:42 -0300

    Add files via upload

---
+++ PhoneOSINT.py
@@ -0,0 +1,18 @@
+import phonenumbers
+from phonenumbers import geocoder
+
+def main():
+	
+	print("\n\nCreado por: RedLineFB")
+	print("\nInstagram: facundo_betancur97\n")
+	
+	num= input("Ingrese un numero: ")
+	funcionnum= phonenumbers.parse(num)	#Se convierte al formato de número de teléfono
+	ubigeo=geocoder.description_for_number(funcionnum,"es") #Para obtener ubicación del número de teléfono
+	print(ubigeo)
+
+if __name__ == '__main__':
+	try:
+		main()
+	except KeyboardInterrupt:
+		exit()
