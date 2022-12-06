e9b8a653aa3404c3245f12425a4b11feb781aa53
Facundo Betancur <114316952+RedLineFB@users.noreply.github.com>
2022-12-06 12:03:22 -0300

    Update PhoneOSINT.py

--- PhoneOSINT.py
+++ PhoneOSINT.py
@@ -6,10 +6,11 @@ def main():
 	print("\n\nCreado por: RedLineFB")
 	print("\nInstagram: facundo_betancur97\n")
 	
-	num= input("Ingrese un numero: ")
+	num= input("Ingrese un numero con el(+) incluido: ")
 	funcionnum= phonenumbers.parse(num)	#Se convierte al formato de número de teléfono
 	ubigeo=geocoder.description_for_number(funcionnum,"es") #Para obtener ubicación del número de teléfono
-	print(ubigeo)
+	print("\nLa geolocalizacion del numero es de: ", ubigeo)
+
 
 if __name__ == '__main__':
 	try:
